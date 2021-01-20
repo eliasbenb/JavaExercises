3516ac757726daeb90a618bdec9c204cc4f26275
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-01-20 15:25:15 +0400

    Fixed exercise 12

--- Exercise012/src/App.java
+++ Exercise012/src/App.java
@@ -8,20 +8,22 @@ public class App {
         Scanner in = new Scanner(System.in);
 
         System.out.println("===================================");
-        System.out.println("Input the longitude of point 1:");
-        double long1 = in.nextDouble();
-        System.out.println("===================================");
         System.out.println("Input the latitude of point 1:");
-        double lat1 = in.nextDouble();
+        double lat1 = Math.toRadians(in.nextDouble());
         System.out.println("===================================");
-        System.out.println("Input the longitude of point 2:");
-        double long2 = Math.toRadians(in.nextDouble());
+        System.out.println("Input the longitude of point 1:");
+        double long1 = Math.toRadians(in.nextDouble());
         System.out.println("===================================");
         System.out.println("Input the latitude of point 2:");
         double lat2 = Math.toRadians(in.nextDouble());
         System.out.println("===================================");
+        System.out.println("Input the longitude of point 2:");
+        double long2 = Math.toRadians(in.nextDouble());
+        System.out.println("===================================");
 
-        double dist = 2 * 6371.01 * Math.asin(Math.sqrt(Math.sin(Math.sin((lat2 - lat1) / 2)) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(Math.sin((long2 - long1) / 2))));
+        double x = Math.sin((lat2 - lat1) / 2) * Math.sin((lat2 - lat1) / 2)
+                + Math.cos(lat1) * Math.cos(lat2) * Math.sin((long2 - long1) / 2) * Math.sin((long2 - long1) / 2);
+        double dist = 6371.01 * 2 * Math.atan2(Math.sqrt(x), Math.sqrt(1 - x));
 
         System.out.println("\n\n============================================================");
         System.out.println("The distance between the two points is: " + String.valueOf(dist));
